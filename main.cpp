18c2e399a4db70c4a2fa53d295025783fd05f1bf
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2021-02-28 11:17:23 -0500

    WIP: Separate thread to process image. Performance is decent but the problem is that new snapshots get captured/shown overriding the ones with the drawn boxes

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main()
     std::vector<dlib::rectangle> faces; 
     std::vector<matrix<rgb_pixel>> normalized_faces; 
     std::vector<matrix<float,0,1>> face_descriptors; 
+    std::map<dlib::rectangle, string> faces_and_labels; 
 
     VideoCapture camera(0);
     if (! camera.isOpened())
@@ -61,75 +62,81 @@ int main()
                 }
             });
 
+    std::thread process_snapshot([&grabbed, &snapshot, &snapshot_mutex, &snapshot_rgb, known_faces]() mutable
+            {
+                std::vector<dlib::rectangle> faces; 
+                std::vector<matrix<rgb_pixel>> normalized_faces; 
+                std::vector<matrix<float,0,1>> face_descriptors; 
+                std::map<dlib::rectangle, string> faces_and_labels; 
+
+                while(grabbed)
+                {
+                    { // separate scope so that lock is realease after
+                        // Convert to RGB
+                        cout << "Process thread convert to RGB" << endl;
+                        std::lock_guard<std::mutex> lock(snapshot_mutex);
+                        cout << "Process thread convert to RGB lock acquired" << endl;
+                        cvtColor(snapshot, snapshot_rgb, COLOR_BGR2RGB);
+                        // lock should be released here
+                    }
+                    cv_image<rgb_pixel> img(snapshot_rgb); 
+                    faces = detectFaces(img);
+                    if (faces.size() > 0)
+                    {
+                        normalized_faces = normalize(faces, img);
+                        face_descriptors = convertToVector(normalized_faces);
+                        //cout << "Face descriptor size: "<< face_descriptors.size() << endl;
+                        //Using a regular for loop here because I'm banking on the fact that 
+                        //if a face has index i, it's corresponding face_descriptor will also be at index i
+                        //this seems to be a correct assumption
+                        for (int i = 0; i < face_descriptors.size(); ++i) // one descriptor = one face
+                        {
+                            bool match = false;
+                            for (auto const& known_face : known_faces)
+                            {
+                                //cout << "Distance between detected face and " << known_face.first << ": "<< length(known_face.second - descriptor)  << endl;
+                                float distance = length(known_face.second-face_descriptors[i]);
+                                //cout << "Distance between detected face and " << known_face.first << " is: " << distance << endl;
+                                if (distance < 0.5)
+                                {
+                                    faces_and_labels.insert({faces[i], known_face.first});
+                                    match = true;
+                                    break; // found match leave
+                                }
+                            }
+                            if (!match)
+                            {
+                                faces_and_labels.insert({faces[i], "?????"});
+                            }
+                        }
+                        {
+                            cout << "Process thread draw boxes" << endl;
+                            std::lock_guard<std::mutex> lock(snapshot_mutex);
+                            cout << "Process thread draw boxes lock acquired" << endl;
+                            drawBoxAroundFaces(snapshot, faces_and_labels); // need to find a away to continue main thread from here so that snapshot shown is the one with the drawn boxes
+                        }
+                    }
+                    faces_and_labels.clear();
+                }
+            });
+
     CountsPerSec cps;
     while(grabbed)
     {
-        
-        std::map<dlib::rectangle, string> faces_and_labels; // this var should only have scope within the while loop
-        //detectAndDisplay(snapshot);
-        { // separate scope so that lock is realease after
-            // Convert to RGB
-            cout << "Main thread convert to RGB" << endl;
-            std::lock_guard<std::mutex> lock(snapshot_mutex);
-            cout << "Main thread convert to RGB lock acquired" << endl;
-            cvtColor(snapshot, snapshot_rgb, COLOR_BGR2RGB);
-            // lock should be released here
-        }
-        // convert to dlib style image
-        cv_image<rgb_pixel> img(snapshot_rgb); 
-        faces = detectFaces(img);
-        if (faces.size() > 0)
-        {
-            normalized_faces = normalize(faces, img);
-            face_descriptors = convertToVector(normalized_faces);
-            //cout << "Face descriptor size: "<< face_descriptors.size() << endl;
-            //Using a regular for loop here because I'm banking on the fact that 
-            //if a face has index i, it's corresponding face_descriptor will also be at index i
-            //this seems to be a correct assumption
-            for (int i = 0; i < face_descriptors.size(); ++i) // one descriptor = one face
-            {
-                bool match = false;
-                for (auto const& known_face : known_faces)
-                {
-                    //cout << "Distance between detected face and " << known_face.first << ": "<< length(known_face.second - descriptor)  << endl;
-                    float distance = length(known_face.second-face_descriptors[i]);
-                    //cout << "Distance between detected face and " << known_face.first << " is: " << distance << endl;
-                    if (distance < 0.5)
-                    {
-                        faces_and_labels.insert({faces[i], known_face.first});
-                        match = true;
-                        break; // found match leave
-                    }
-                }
-                if (!match)
-                {
-                    faces_and_labels.insert({faces[i], "?????"});
-                }
-            }
-            {
-                cout << "Main thread draw boxes" << endl;
-                std::lock_guard<std::mutex> lock(snapshot_mutex);
-                cout << "Main thread draw boxes lock acquired" << endl;
-                drawBoxAroundFaces(snapshot, faces_and_labels);
-            }
-        }
         {
             cout << "Main thread put iterations" << endl;
             std::lock_guard<std::mutex> lock(snapshot_mutex);
             cout << "Main thread put iterations lock acquired" << endl;
             put_iterations_per_sec(snapshot, cps.counts_per_sec());
-        }
-        {
-            cout << "Main thread display snapshot" << endl;
-            std::lock_guard<std::mutex> lock(snapshot_mutex);
-            cout << "Main thread display snapshot lock acquired" << endl;
             imshow( "Capture - Face detection", snapshot);
         }
+        std::this_thread::yield();
         cps.increment();
         waitKey(1);
     }
 
     capture_thread.join(); // never reached? because I sigint to terminate the program
+    process_snapshot.join(); // never reached? because I sigint to terminate the program
     
     return 0;
 }
